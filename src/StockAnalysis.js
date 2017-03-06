3c602dd848477e2c79298ed23078960b67fe79f2
rachelmad <rachelvmadrigal@gmail.com>
2017-03-05 23:14:39 -0600

    Styled bar graph

--- src/StockAnalysis.js
+++ src/StockAnalysis.js
@@ -39,7 +39,7 @@ export default class StockMain extends Component {
 	createChart() {
 		var ctx = document.getElementById("chart");
 		var chart = new Chart(ctx, {
-		    type: 'bar',
+		    type: 'horizontalBar',
 		    data: {
 		        labels: [
 		        	"Analyst Recommendations", 
@@ -58,28 +58,13 @@ export default class StockMain extends Component {
 		            	this.state.analysis.pegAnalysis, 
 		            	this.state.analysis.companyIndustryEarnings,
 		            	this.state.analysis.priceVsBookValue],
-		            backgroundColor: [
-		                'rgba(255, 99, 132, 0.2)',
-		                'rgba(54, 162, 235, 0.2)',
-		                'rgba(255, 206, 86, 0.2)',
-		                'rgba(75, 192, 192, 0.2)',
-		                'rgba(153, 102, 255, 0.2)',
-		                'rgba(255, 159, 64, 0.2)',
-		                'rgba(255, 159, 64, 0.2)'
-		            ],
-		            borderColor: [
-		                'rgba(255,99,132,1)',
-		                'rgba(54, 162, 235, 1)',
-		                'rgba(255, 206, 86, 1)',
-		                'rgba(75, 192, 192, 1)',
-		                'rgba(153, 102, 255, 1)',
-		                'rgba(255, 159, 64, 1)',
-		                'rgba(255, 159, 64, 0.2)'
-		            ],
-		            borderWidth: 1
+		            backgroundColor: 'rgb(66, 133, 244)'
 		        }]
 		    },
 		    options: {
+		    	legend: {
+		    		display: false
+		    	},
 		        scales: {
 		            yAxes: [{
 		                ticks: {
